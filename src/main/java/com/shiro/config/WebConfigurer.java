cb327e6076b2491536c706a8d73788c13f0e8e43
guiguoliang <584961991@qq.com>
2020-07-28 10:13:35 +0800

    修改静态文件路径

--- src/main/java/com/shiro/config/WebConfigurer.java
+++ src/main/java/com/shiro/config/WebConfigurer.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
 
-@Configuration
+/*@Configuration
 public class WebConfigurer implements WebMvcConfigurer {
  
   @Resource
@@ -30,7 +30,8 @@ public class WebConfigurer implements WebMvcConfigurer {
     	 .excludePathPatterns("/","/user/login","/kickout","/api/**")
     	 .excludePathPatterns("/druid/**")
     	 .excludePathPatterns("/page")
-         .excludePathPatterns("/plugins/**")
+         .excludePathPatterns("/static/**")
     	 .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
   }
 }
+*/
